f1268caff23803392db7c186b88cae1a8f145eb0
Siva Kumar <sivakumar9399959803@gmail.com>
2024-01-19 11:14:25 +0530

    roboshop-jenkins

---
+++ vars/mainci.groovy
@@ -0,0 +1,41 @@
+def call() {
+    node('workstation') {
+
+        if (env.cibuild == "java") {
+
+            stage('Build') {
+                sh 'mvn package'
+
+            } elseif (env.cibuild == "nodejs") {
+
+                stage('Build') {
+                    sh 'npm install'
+
+                }
+            }
+        }
+
+        stage('Unit Tests') {
+            echo 'Unit Tests'
+        }
+
+        stage('Code Analysis') {
+            echo 'Code Analysis'
+            // sh 'sonar-scanner -Dsonar.host.url=http://172.31.30.244:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
+        }
+
+        stage('Security Scans') {
+            echo 'Security Scans'
+        }
+
+
+        if(env.TAG_NAME ==~ ".*") {
+            stage('Publish A Article') {
+                echo 'Publish A Article'
+                }
+            }
+
+    }
+
+}
+
