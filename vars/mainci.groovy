dcc7928ca7e1d26ccb4647177e42cb89567899d9
Siva Kumar <sivakumar9399959803@gmail.com>
2024-02-23 16:44:33 +0530

    roboshop-jenkins

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -55,7 +55,7 @@ def call() {
                      sh 'mv target/${component}-1.0.jar ${component}.jar'
                      sh 'rm -rf pom.xml src target'
                     }
-                sh 'rm -f jenkinsfile'
+                sh 'rm -f Jenkinsfile'
                 sh 'echo ${TAG_NAME} > VERSION'
                 sh 'zip -r ${component}-${TAG_NAME}.zip *'
                 sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.6.181:8081/repository/${component}/${component}-${TAG_NAME}.zip'
